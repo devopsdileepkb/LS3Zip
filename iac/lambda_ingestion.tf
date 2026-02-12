139a2a946c640c630af685502badfb1d4fa4f790
devopsdileepkb <dilee8217@gmail.com>
2026-02-12 23:28:50 +0530

    Update lambda_ingestion.tf

--- iac/lambda_ingestion.tf
+++ iac/lambda_ingestion.tf
@@ -35,6 +35,15 @@ resource "aws_lambda_function" "zip_csv_lambda" {
   }
 }
 
+# Permission for S3 to invoke Lambda
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.zip_csv_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.my_bucket.arn
+}
+
 resource "aws_s3_bucket_notification" "bucket_notify" {
   bucket = var.bucket_name
 
@@ -45,8 +54,12 @@ resource "aws_s3_bucket_notification" "bucket_notify" {
     filter_suffix       = ".csv"
   }
 
-  depends_on = [aws_lambda_function.zip_csv_lambda]
+  depends_on = [
+    aws_lambda_function.zip_csv_lambda,
+    aws_lambda_permission.allow_s3
+  ]
 }
+
 # EventBridge rule to trigger Lambda daily at 1 AM UTC
 resource "aws_cloudwatch_event_rule" "daily_trigger" {
   name                = "zip-csv-daily-${var.environment}"
